5acc65f1369d4e487c1c407815319877e16b9039
Monkey Liang <i@aq741.com>
2017-01-07 07:38:30 +0800

    update assignment 1

--- chapter1/boot.asm
+++ chapter1/boot.asm
@@ -5,12 +5,12 @@
 	call	DispStr			; 调用显示字符串例程
 	jmp	$			; 无限循环
 DispStr:
-	mov	ax, BootMessage
-	mov	bp, ax			; ES:BP = 串地址
+	;mov	ax, BootMessage
+	mov	bp, BootMessage			; ES:BP = 串地址
 	mov	cx, 16			; CX = 串长度
 	mov	ax, 01301h		; AH = 13,  AL = 01h
 	mov	bx, 000ch		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
-	mov	dl, 0
+	;mov	dl, 0
 	int	10h			; 10h 号中断
 	ret
 BootMessage:		db	"Hello, OS world!"
