b1ce1b5f338d72a3992996bcc60a71a82a8abada
DeepAQ <i@aq741.com>
2017-05-04 19:06:31 +0800

    add a stub

---
+++ MemoryManagement/call.c
@@ -0,0 +1,21 @@
+#include "call.h"
+
+void init() {
+
+}
+
+int read(data_unit *data, v_address address, m_pid_t pid) {
+    return -1;
+}
+
+int write(data_unit data, v_address address, m_pid_t pid) {
+    return -1;
+}
+
+int allocate(v_address *address, m_size_t size, m_pid_t pid) {
+    return -1;
+}
+
+int free(v_address address, m_pid_t pid) {
+    return -1;
+}
