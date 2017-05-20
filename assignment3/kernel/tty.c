ce82b88b2ac372ce4ef5b8033077a03c286d10fc
DeepAQ <i@aq741.com>
2017-05-21 05:17:23 +0800

    support search

--- assignment3/kernel/tty.c
+++ assignment3/kernel/tty.c
@@ -18,8 +18,13 @@
 #define SCREEN_HEIGHT 25
 #define TEXT_SIZE V_MEM_SIZE / 2
 
+PRIVATE u8 *vmem = V_MEM_BASE;
 PRIVATE char text[TEXT_SIZE];
 PRIVATE int text_pos;
+PRIVATE int state;
+PRIVATE char pattern[TEXT_SIZE];
+PRIVATE int pattern_pos;
+PRIVATE int cursor_pos;
 
 PRIVATE void render();
 
@@ -31,7 +36,7 @@ PUBLIC void task_tty()
     // Init console
     disp_str("Loading console...\n");
     memset(text, 0, TEXT_SIZE);
-    text_pos = 0;
+    text_pos = state = 0;
     render();
     // Poll keyboard events
     while (1) {
@@ -46,12 +51,12 @@ PRIVATE void set_cursor(unsigned int position)
     out_byte(CRTC_DATA_REG, (position >> 8) & 0xFF);
     out_byte(CRTC_ADDR_REG, CURSOR_L);
     out_byte(CRTC_DATA_REG, position & 0xFF);
+    cursor_pos = position;
     enable_int();
 }
 
 PRIVATE void render()
 {
-    u8 *vmem = V_MEM_BASE;
     // clear vmem
     for (int i = 0; i < V_MEM_SIZE / 2; i++) {
         vmem[i * 2] = 0;
@@ -59,11 +64,11 @@ PRIVATE void render()
     }
     // start rendering
     int row = 0, col = 0;
-    for (int i = 0; i < TEXT_SIZE; i++) {
+    for (int i = 0; i < text_pos; i++) {
         if (text[i] == 0) break;
         switch (text[i]) {
         case '\n':
-            row++;
+            row = (row + 1) % SCREEN_HEIGHT;
             col = 0;
             break;
         case '\t':
@@ -71,15 +76,24 @@ PRIVATE void render()
             break;
         default:
             vmem[(row * SCREEN_WIDTH + col) * 2] = text[i];
-            vmem[(row * SCREEN_WIDTH + col) * 2 + 1] = 0x0f;
+            if (state == 2 && text[i] == pattern[0]) {
+                int j;
+                for (j = 1; j < pattern_pos && i + j < text_pos && text[i + j] == pattern[j]; j++);
+                if (j == pattern_pos)
+                    for (; j > 0; j--) vmem[(row * SCREEN_WIDTH + col + j) * 2 - 1] = 0x0e;
+            }
             col++;
             break;
         }
         if (col >= SCREEN_WIDTH) {
-            row++;
+            row = (row + 1) % SCREEN_HEIGHT;
             col = col % SCREEN_WIDTH;
         }
-        row %= SCREEN_HEIGHT;
+    }
+    for (int i = 0; state == 2 && i < pattern_pos; i++) {
+        vmem[(row * SCREEN_WIDTH + col) * 2] = pattern[i];
+        vmem[(row * SCREEN_WIDTH + col) * 2 + 1] = 0x0e;
+        col++;
     }
     set_cursor(row * SCREEN_WIDTH + col);
 }
@@ -89,29 +103,52 @@ PRIVATE void render()
  *======================================================================*/
 PUBLIC void in_process(u32 key)
 {
-    if (!(key & FLAG_EXT)) {
-        text[text_pos++] = key & 0xFF;
-        render();
-    }
-    else {
-        int raw_code = key & MASK_RAW;
-        switch(raw_code) {
-        case ENTER:
-            text[text_pos++] = '\n';
+    if (state == 0) {
+        if (!(key & FLAG_EXT)) {
+            text[text_pos++] = key & 0xFF;
             render();
-            break;
-        case TAB:
-            text[text_pos++] = '\t';
-            render();
-            break;
-        case BACKSPACE:
-            if (text_pos > 0) {
-                text[--text_pos] = 0;
+        } else {
+            switch(key & MASK_RAW) {
+            case ENTER:
+                text[text_pos++] = '\n';
+                render();
+                break;
+            case TAB:
+                text[text_pos++] = '\t';
+                render();
+                break;
+            case BACKSPACE:
+                if (text_pos > 0) {
+                    text[--text_pos] = 0;
+                    render();
+                }
+                break;
+            case ESC:
+                memset(pattern, 0, TEXT_SIZE);
+                pattern_pos = 0;
+                state = 1;
+            }
+        }
+        text_pos %= TEXT_SIZE;
+    } else if (state == 1) {
+        if (!(key & FLAG_EXT)) {
+            pattern[pattern_pos++] = key & 0xFF;
+            vmem[cursor_pos * 2] = key & 0xFF;
+            vmem[cursor_pos * 2 + 1] = 0x0e;
+            set_cursor(++cursor_pos);
+        } else {
+            switch (key & MASK_RAW) {
+            case ENTER:
+                state = 2;
+                render();
+                break;
+            case ESC:
+                state = 0;
                 render();
             }
-            break;
         }
+    } else if (state == 2 && (key & FLAG_EXT) && ((key & MASK_RAW) == ESC)) {
+        state = 0;
+        render();
     }
-    text_pos %= TEXT_SIZE;
 }
-
